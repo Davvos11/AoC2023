ea426824c09afa417a39df785a571447a7b3fcbb
David Vos <vosdavid2@gmail.com>
2023-12-03 17:55:00 +0100

    added readme and updated main code

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Advent of Code 2023
+#### By Davvos11, in Rust
+
+You can use cargo to run a specific day (or all days):
+
+```shell
+# Run a specific day:
+cargo run -- 1
+# Run the latest day:
+cargo run -- 0
+# Run all days:
+cargo run -- a
+# Get a text prompt to specify a day:
+cargo run 
+```
--- src/main.rs
+++ src/main.rs
@@ -8,30 +8,37 @@ mod day02;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let day: usize;
+    let day: &str;
+    let mut day_arg = String::new();
 
     if let Some(day_arg) = args.get(1) {
-        day = day_arg.parse().expect("Please provide the day as a number");
+        day = day_arg;
     } else {
-        let mut day_arg = String::new();
-        println!("Input day (0 to run all): ");
+        println!("Input day (0 to run latest, a to run all): ");
         io::stdin()
             .read_line(&mut day_arg)
             .expect("Failed to read line");
         println!();
-        day = day_arg.trim().parse().expect("Please provide the day as a number")
+        day = day_arg.trim();
     }
 
     let days = [day01, day02];
 
     match day {
-        0 => {
+        "a" => {
             for (i, day) in days.iter().enumerate() {
                 println!("Day {}:", i + 1);
                 day();
             }
         }
+        "0" => {
+            if let Some(day) = days.last() {
+                println!("Day {}:", days.len());
+                day();
+            }
+        }
         num => {
+            let num: usize = num.parse().expect("Please provide the day as a number, '0' for latest or 'a' for all");
             println!("Day {}:", num);
             if let Some(day) = days.get(num - 1) {
                 day();
