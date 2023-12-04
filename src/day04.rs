9172b5120f7e661ad4ed7e2091b326f434fead7b
David Vos <vosdavid2@gmail.com>
2023-12-04 19:07:37 +0100

    small optimisations to day 4

--- src/day04.rs
+++ src/day04.rs
@@ -1,48 +1,39 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 use std::fs::read_to_string;
 
 pub fn day04() {
     let input = read_to_string("static/input04.txt").expect("Cannot read input file");
+    let input = input.trim();
 
     let mut result1 = 0;
-    let mut copies = HashMap::new();
+    let mut copies = vec![1_usize; input.lines().count()];
 
-    for line  in input.lines() {
-        let left = line.split(':').next().unwrap();
-        let mut right = line.strip_prefix(left).unwrap()[2..].split('|');
+    for (i, line)  in input.lines().enumerate() {
+        let mut numbers = line.split(|c| c == '|' || c == ':');
+        numbers.next(); // Ignore card number (use enumerate value instead)
 
-        let card_number: usize = get_card_num(left);
-        let winning_nums= string_to_int_set(right.next().unwrap());
-        let nums = string_to_int_set(right.next().unwrap());
+        let winning_nums: HashSet<usize> = string_to_int_set(numbers.next().unwrap())
+            .collect();
+        let nums = string_to_int_set(numbers.next().unwrap());
 
-        let wins = winning_nums.intersection(&nums).count();
+        let wins = nums.filter(|x| winning_nums.contains(x)).count();
         if wins > 0 {
             result1 += 2_usize.pow((wins - 1) as u32);
         }
 
         // Get the amount of copies that the current card has
-        let current_copies = *copies.entry(card_number).or_insert(1);
+        let current_copies = copies[i];
 
         // Loop through the cards below this one (for the amount of wins of this card)
-        for card_to_copy in card_number+1..=card_number+wins {
-            // Add 1 to the cards for every copy of this card
-            let count = copies.entry(card_to_copy).or_insert(1);
-            *count += current_copies;
-        }
+        copies[i+1..=i+wins].iter_mut().for_each(|card| *card += current_copies);
     }
 
-    let result2: usize = copies.values().sum();
+    let result2: usize = copies.iter().sum();
 
     println!("Part 1: {result1}");
     println!("Part 2: {result2}");
 }
 
-fn string_to_int_set(string: &str) -> HashSet<usize> {
-    string.split_whitespace().map(|s|s.parse().unwrap()).collect()
-}
-
-fn get_card_num(string: &str) -> usize {
-    string.chars()
-        .filter(|c| c.is_ascii_digit())
-        .collect::<String>().parse().unwrap()
+fn string_to_int_set(string: &str) -> impl Iterator<Item = usize> + '_ {
+    string.split_whitespace().filter_map(|s|s.parse().ok())
 }
