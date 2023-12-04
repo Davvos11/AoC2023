13f2ceeb029c30a049c5a612b47ebb2f16b2d711
David Vos <vosdavid2@gmail.com>
2023-12-04 12:55:56 +0100

    removed regex from day 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,48 +1,41 @@
 use std::fs::read_to_string;
-use regex::Regex;
 
 // (r,g,b)
-const GAME: [i32; 3] = [12, 13, 14];
+const GAME: [usize; 3] = [12, 13, 14];
 
 pub fn day02() {
     let input = read_to_string("static/input02.txt").expect("Cannot read input file");
-    let id_pattern = Regex::new(r"Game (\d+): ").unwrap();
-    let cube_pattern = Regex::new(r"(\d+) (\w+)").unwrap();
 
     let mut result_01 = 0;
     let mut result_02 = 0;
 
     for line in input.lines() {
-        let capt = id_pattern.captures(line).unwrap();
-        let id: i32 = capt.get(1).unwrap().as_str().parse().unwrap();
-        let prefix = capt.get(0).unwrap().as_str();
-        let line = line.strip_prefix(prefix).unwrap();
+        let parts: Vec<&str> = line.split(|c| c == ':' || c == ';')
+            .map(|s| s.trim())
+            .collect();
+        let id = get_game_num(parts[0]);
 
-        let sets = line.split(',');
+        let sets = &parts[1..];
         let mut possble = true;
         let mut minimum = [0, 0, 0];
 
         for set in sets {
-            for cube in cube_pattern.captures_iter(set) {
-                if let Some(colour) = cube.get(2) {
-                    if let Some(amount) = cube.get(1) {
-                        let amount: i32 = amount.as_str().parse().unwrap();
-                        let index = match colour.as_str() {
-                            "red" => 0,
-                            "green" => 1,
-                            "blue" => 2,
-                            _ => { panic!("Colour not found") }
-                        };
+            for cube in set.split(',') {
+                let (colour, amount) = get_colour_and_amount(cube);
+                let index = match colour.as_str() {
+                    "red" => 0,
+                    "green" => 1,
+                    "blue" => 2,
+                    _ => { panic!("Colour not found") }
+                };
 
-                        // Part 1
-                        if amount > GAME[index] {
-                            possble = false;
-                        }
-                        // Part 2
-                        if minimum[index] < amount {
-                            minimum[index] = amount;
-                        }
-                    }
+                // Part 1
+                if amount > GAME[index] {
+                    possble = false;
+                }
+                // Part 2
+                if minimum[index] < amount {
+                    minimum[index] = amount;
                 }
             }
         }
@@ -50,8 +43,24 @@ pub fn day02() {
         if possble {
             result_01 += id;
         }
-        result_02 += minimum.iter().product::<i32>();
+        result_02 += minimum.iter().product::<usize>();
     }
 
     println!("Part 1: {result_01}\nPart 2: {result_02}");
+}
+
+fn get_game_num(string: &str) -> usize {
+    string.chars()
+        .filter(|c| c.is_ascii_digit())
+        .collect::<String>().parse().unwrap()
+}
+
+fn get_colour_and_amount(string: &str) -> (String, usize) {
+    let (digits, non_digits): (String, String) = string.chars()
+        .partition(|c| c.is_ascii_digit());
+
+    let colour = non_digits.trim();
+    let amount = digits.parse().unwrap();
+
+    (colour.to_string(), amount)
 }
