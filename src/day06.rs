fb9eacc0d05be2d31d3f051be1fc4ef75da10a74
David Vos <vosdavid2@gmail.com>
2023-12-06 13:32:56 +0100

    day 6 part 2 ("slow")

--- src/day06.rs
+++ src/day06.rs
@@ -2,8 +2,8 @@ use crate::utils::parse_spaced_string;
 
 pub fn day06(input: &str) -> (i32, i32) {
     let mut result1 = 1;
-    let result2 = 0;
 
+    // Part 1:
     let mut lines = input.lines();
     let times = parse_spaced_string(lines.next().unwrap());
     let distance = parse_spaced_string(lines.next().unwrap());
@@ -16,5 +16,18 @@ pub fn day06(input: &str) -> (i32, i32) {
         }).count();
     }
 
-    (result1 as i32, result2)
+    // Part 2:
+    let mut lines = input.lines();
+    let time = get_number(lines.next().unwrap());
+    let record = get_number(lines.next().unwrap());
+
+    let result2 = (0..time).filter(|&time_held| {
+        time_held * (time - time_held) > record
+    }).count();
+
+    (result1 as i32, result2 as i32)
+}
+
+fn get_number(string: &str) -> u64 {
+    string.chars().filter(|c|c.is_ascii_digit()).collect::<String>().parse().unwrap()
 }
