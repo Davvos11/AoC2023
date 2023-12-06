3f7abc7fd4e4108382c4f7bdc8f35a9eb1a63d11
David Vos <vosdavid2@gmail.com>
2023-12-06 13:54:36 +0100

    move shared logic

--- src/day06.rs
+++ src/day06.rs
@@ -6,26 +6,26 @@ pub fn day06(input: &str) -> (i32, i32) {
     // Part 1:
     let mut lines = input.lines();
     let times = parse_spaced_string(lines.next().unwrap());
-    let distance = parse_spaced_string(lines.next().unwrap());
-    let races: Vec<(u32, u32)> = times.zip(distance).collect();
+    let records = parse_spaced_string(lines.next().unwrap());
+    let races = times.zip(records);
 
-    for (time, record) in races {
-        // distance = time_held * (time - time_held)
-        result1 *= (0..time).filter(|&time_held| {
-            time_held * (time - time_held) > record
-        }).count();
-    }
+    let result1: usize = races.map(|(t,r)|calculate_race(t,r)).product();
 
     // Part 2:
     let mut lines = input.lines();
     let time = get_number(lines.next().unwrap());
     let record = get_number(lines.next().unwrap());
 
-    let result2 = (0..time).filter(|&time_held| {
-        time_held * (time - time_held) > record
-    }).count();
+    let result2 = calculate_race(time, record);
 
     (result1 as i32, result2 as i32)
+}
+
+fn calculate_race(time: u64, record: u64) -> usize {
+    // distance = time_held * (time - time_held)
+    (0..time).filter(|&time_held| {
+        time_held * (time - time_held) > record
+    }).count()
 }
 
 fn get_number(string: &str) -> u64 {
