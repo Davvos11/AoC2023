533cbb502a5473b667722979d75a00323cf0602f
David Vos <vosdavid2@gmail.com>
2023-12-06 14:42:03 +0100

    day 6 part 2 mathematically

--- src/day06.rs
+++ src/day06.rs
@@ -1,15 +1,13 @@
 use crate::utils::parse_spaced_string;
 
 pub fn day06(input: &str) -> (i32, i32) {
-    let mut result1 = 1;
-
     // Part 1:
     let mut lines = input.lines();
     let times = parse_spaced_string(lines.next().unwrap());
     let records = parse_spaced_string(lines.next().unwrap());
     let races = times.zip(records);
 
-    let result1: usize = races.map(|(t,r)|calculate_race(t,r)).product();
+    let result1: usize = races.map(|(t, r)| calculate_race(t, r)).product();
 
     // Part 2:
     let mut lines = input.lines();
@@ -21,13 +19,28 @@ pub fn day06(input: &str) -> (i32, i32) {
     (result1 as i32, result2 as i32)
 }
 
-fn calculate_race(time: u64, record: u64) -> usize {
+// Naive / iterative approach
+#[allow(dead_code)]
+fn calculate_race_(time: u64, record: u64) -> usize {
     // distance = time_held * (time - time_held)
     (0..time).filter(|&time_held| {
         time_held * (time - time_held) > record
     }).count()
 }
 
+
+// Parabola approach
+fn calculate_race(time: u64, record: u64) -> usize {
+    // D = b^2 - 4ac, a = 1, b = -time, c = intersection
+    let d = time.pow(2) as f64 - 4.0 * record as f64;
+    // (assuming d >= 0)
+    // x = -b +- sqrt(D) / 2a
+    let x1 = (time as f64 - d.sqrt()) / 2.0;
+    let x2 = (time as f64 + d.sqrt()) / 2.0;
+
+    (x2.ceil() - x1.floor() - 1.0) as usize
+}
+
 fn get_number(string: &str) -> u64 {
-    string.chars().filter(|c|c.is_ascii_digit()).collect::<String>().parse().unwrap()
+    string.chars().filter(|c| c.is_ascii_digit()).collect::<String>().parse().unwrap()
 }
