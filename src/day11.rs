9b9fce0cc9b6720741c871d5ee4482bc8637d570
David Vos <vosdavid2@gmail.com>
2023-12-11 18:10:10 +0100

    day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 pub fn day11(input: &str) -> (isize, isize) {
     let mut result1 = 0;
-    let result2 = 0;
+    let mut result2 = 0;
 
     // Get rows anc columns that don't contain galaxies
     let empty_rows: HashSet<_> =
@@ -18,7 +18,8 @@ pub fn day11(input: &str) -> (isize, isize) {
         }).collect();
 
     // Get galaxy coordinates
-    let mut galaxies = Vec::new();
+    let mut galaxies1 = Vec::new();
+    let mut galaxies2 = Vec::new();
     let mut i_offset = 0;
     for (i, line) in input.lines().enumerate() {
         if empty_rows.contains(&i) { i_offset += 1 }
@@ -28,20 +29,24 @@ pub fn day11(input: &str) -> (isize, isize) {
             if empty_cols.contains(&j) { j_offset += 1 }
 
             if char == '#' {
-                galaxies.push((i + i_offset, j + j_offset))
+                galaxies1.push((i + i_offset, j + j_offset));
+                galaxies2.push((i + i_offset * 999_999, j + j_offset * 999_999));
             }
         }
     }
 
+    let galaxies = galaxies1.iter().zip(&galaxies2);
+
     // Calculate distances
-    for (i, &galaxy1) in galaxies.iter().enumerate() {
-        for (j, &galaxy2) in galaxies.iter().enumerate() {
+    for (i, galaxy1) in galaxies.clone().enumerate() {
+        for (j, galaxy2) in galaxies.clone().enumerate() {
             if i >= j { continue }
-            result1 += manhattan_distance(galaxy1, galaxy2);
+            result1 += manhattan_distance(*galaxy1.0, *galaxy2.0);
+            result2 += manhattan_distance(*galaxy1.1, *galaxy2.1);
         }
     }
 
-    (result1 as isize, result2)
+    (result1 as isize, result2 as isize)
 }
 
 fn manhattan_distance((x1, y1): (usize, usize), (x2, y2): (usize, usize)) -> usize {
