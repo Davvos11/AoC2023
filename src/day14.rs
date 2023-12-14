d4a7ecab425a66941c95a9873ca35b1bb79fda20
David Vos <vosdavid2@gmail.com>
2023-12-14 18:21:44 +0100

    day 14 part 3, by pattern recognition

--- src/day14.rs
+++ src/day14.rs
@@ -5,7 +5,6 @@ use Rock::*;
 pub fn day14(input: &str) -> (isize, isize) {
     let rows = input.lines().count();
     let cols = input.lines().next().unwrap().len();
-    let mut memo = Memo::new();
 
     // Parse rocks
     // rocks[col, row] = rock
@@ -23,20 +22,83 @@ pub fn day14(input: &str) -> (isize, isize) {
     }
 
 
-    rocks = memo.tilt(&rocks, cols, rows);
+    rocks = tilt(&rocks, cols, rows);
     let result1 = get_load(&rocks, rows);
 
-    // Cycle n times
-    // let mut rs = Vec::new();
-    for i in 0..1000000000 {
-        rocks = memo.cycle(rocks, cols, rows);
-    }
+    let pattern = Pattern::new(rocks, cols, rows);
 
-    // dbg!(rs);
-
-    let result2 = get_load(&rocks, rows);
+    let result2 = pattern.get(1000000000 - 1);
 
     (result1 as isize, result2 as isize)
+}
+
+struct Pattern {
+    initial: Vec<usize>,
+    initial_size: usize,
+    pattern: Vec<usize>,
+    pattern_size: usize,
+}
+
+impl Pattern {
+    /// The result keeps (mostly) lowering until it reaches a pattern
+    /// see: https://i.imgur.com/w0q3ztl.png
+    /// This function will provide that pattern
+    fn new(rocks: BTreeMap<(usize, usize), Rock>, cols: usize, rows: usize) -> Self {
+        let mut initial = Vec::new();
+        let mut pattern = Vec::new();
+        let mut rocks = rocks;
+        // Keep calculating if we are still going down
+        for i in 0.. {
+            rocks = cycle(rocks, cols, rows);
+            initial.push(get_load(&rocks, rows));
+            if i >= 5 && i % 5 == 0 {
+                let slope = initial[i] as isize - initial[i-5] as isize;
+                if -5 < slope {
+                    dbg!(i);
+                    break;
+                }
+            }
+        }
+        // Calculate a large number of times
+        let mut buffer = Vec::new();
+        for _ in 0..100 {
+            rocks = cycle(rocks, cols, rows);
+            buffer.push(get_load(&rocks, rows));
+        }
+        // Try to find the pattern
+        for window_size in 1.. {
+            if window_size > 50 { panic!("Could not find") }
+            let mut windows = buffer.chunks_exact(window_size);
+            if let Some(first) = windows.next() {
+                let mut repeats = false;
+                for window in windows {
+                    repeats = first == window;
+                    if !repeats {
+                        break;
+                    }
+                }
+                if repeats {
+                    pattern = Vec::from(first);
+                    break;
+                }
+            }
+        }
+
+        Self {
+            initial_size: initial.len(),
+            pattern_size: pattern.len(),
+            initial,
+            pattern,
+        }
+    }
+
+    fn get(&self, i: usize) -> usize {
+        if i < self.initial_size {
+            self.initial[i]
+        } else {
+            self.pattern[(i - self.initial_size) % self.pattern_size]
+        }
+    }
 }
 
 fn get_load(rocks: &BTreeMap<(usize, usize), Rock>, rows: usize) -> usize {
@@ -45,88 +107,51 @@ fn get_load(rocks: &BTreeMap<(usize, usize), Rock>, rows: usize) -> usize {
     }).sum()
 }
 
-struct Memo {
-    tilt_memo: HashMap<BTreeMap<(usize, usize), Rock>, BTreeMap<(usize, usize), Rock>>,
-    cycle_memo: HashMap<BTreeMap<(usize, usize), Rock>, BTreeMap<(usize, usize), Rock>>,
-    rotate_memo: HashMap<BTreeMap<(usize, usize), Rock>, BTreeMap<(usize, usize), Rock>>,
+fn cycle(rocks: BTreeMap<(usize, usize), Rock>, cols: usize, rows: usize) -> BTreeMap<(usize, usize), Rock> {
+    let mut rocks = rocks;
+
+    rocks = tilt(&rocks, cols, rows);
+    rocks = rotate(&rocks, cols);
+    rocks = tilt(&rocks, cols, rows);
+    rocks = rotate(&rocks, rows);
+    rocks = tilt(&rocks, cols, rows);
+    rocks = rotate(&rocks, cols);
+    rocks = tilt(&rocks, cols, rows);
+    rocks = rotate(&rocks, rows);
+    rocks
 }
 
-impl Memo {
-    fn new() -> Self {
-        Self {
-            tilt_memo: HashMap::new(),
-            cycle_memo: HashMap::new(),
-            rotate_memo: HashMap::new(),
-        }
-    }
+fn tilt(rocks: &BTreeMap<(usize, usize), Rock>, width: usize, height: usize) -> BTreeMap<(usize, usize), Rock> {
+    let mut rocks = rocks.clone();
 
-    fn cycle(&mut self, rocks: BTreeMap<(usize, usize), Rock>, cols: usize, rows: usize) -> BTreeMap<(usize, usize), Rock> {
-        let rocks_before = rocks.clone();
-        if let Some(result) = self.cycle_memo.get(&rocks_before) {
-            return result.clone()
-        }
-
-        let mut rocks = rocks;
-
-        rocks = self.tilt(&rocks, cols, rows);
-        rocks = self.rotate(&rocks, cols);
-        rocks = self.tilt(&rocks, cols, rows);
-        rocks = self.rotate(&rocks, rows);
-        rocks = self.tilt(&rocks, cols, rows);
-        rocks = self.rotate(&rocks, cols);
-        rocks = self.tilt(&rocks, cols, rows);
-        rocks = self.rotate(&rocks, rows);
-
-        self.cycle_memo.insert(rocks_before, rocks.clone());
-        rocks
-    }
-
-    fn tilt(&mut self, rocks: &BTreeMap<(usize, usize), Rock>, width: usize, height: usize) -> BTreeMap<(usize, usize), Rock> {
-        let rocks_before = rocks.clone();
-        if let Some(result) = self.tilt_memo.get(&rocks_before) {
-            return result.clone()
-        }
-
-        let mut rocks = rocks_before.clone();
-
-        for c in 0..width {
-            let mut next_empty = 0;
-            for r in 0..height {
-                if let Some(&rock) = rocks.get(&(c, r)) {
-                    match rock {
-                        Round => {
-                            if next_empty < r {
-                                rocks.insert((c, next_empty), rock);
-                                rocks.remove(&(c, r));
-                            }
-                            if next_empty <= r {
-                                next_empty += 1;
-                            }
+    for c in 0..width {
+        let mut next_empty = 0;
+        for r in 0..height {
+            if let Some(&rock) = rocks.get(&(c, r)) {
+                match rock {
+                    Round => {
+                        if next_empty < r {
+                            rocks.insert((c, next_empty), rock);
+                            rocks.remove(&(c, r));
                         }
-                        Cube => { next_empty = r + 1; }
+                        if next_empty <= r {
+                            next_empty += 1;
+                        }
                     }
+                    Cube => { next_empty = r + 1; }
                 }
             }
         }
-
-        self.tilt_memo.insert(rocks_before, rocks.clone());
-        rocks
     }
 
-    fn rotate(&mut self, map: &BTreeMap<(usize, usize), Rock>, cols: usize) -> BTreeMap<(usize, usize), Rock> {
-        if let Some(map) = self.rotate_memo.get(map) {
-            return map.clone();
-        }
-
-        let result: BTreeMap<_, Rock> = map.iter().map(|(&(i, j), &value)| {
-            ((cols - 1 - j, i), value)
-        }).collect();
-
-        self.rotate_memo.insert(map.clone(), result.clone());
-        result
-    }
+    rocks
 }
 
+fn rotate(map: &BTreeMap<(usize, usize), Rock>, cols: usize) -> BTreeMap<(usize, usize), Rock> {
+    map.iter().map(|(&(i, j), &value)| {
+        ((cols - 1 - j, i), value)
+    }).collect()
+}
 
 
 #[derive(Debug, Eq, PartialEq, Copy, Clone, Hash)]
