6f04c21081fcf1e800ec591207f889a7c50836e2
David Vos <vosdavid2@gmail.com>
2023-12-15 10:42:51 +0100

    day 15 part 2

--- src/day15.rs
+++ src/day15.rs
@@ -1,9 +1,42 @@
-pub fn day15(input: &str) -> (isize, isize) {
-    let result1: u32 = input.trim().split(',')
-        .map(|s|
-            s.chars().fold(0, |acc, c| ((acc + c as u32) * 17) % 256)
-        ).sum();
-    let result2 = 0;
+use std::collections::HashMap;
+use indexmap::IndexMap;
 
-    (result1 as isize, result2)
+pub fn day15(input: &str) -> (isize, isize) {
+    let hash = |s: &str|
+        s.chars().fold(0, |acc, c| ((acc + c as usize) * 17) % 256);
+    let steps = input.trim().split(',');
+
+    // Part 1:
+    let result1: usize = steps.clone().map(hash).sum();
+
+    // Part 2
+    let mut boxes: HashMap<_, IndexMap<&str, _>> = HashMap::new();
+    for step in steps {
+        let mut split = step.split(|c| c == '-'|| c =='=');
+        let label = split.next().unwrap();
+        let box_id = hash(label);
+        match step.strip_prefix(label).unwrap().chars().next().unwrap() {
+            '-' => {
+                if let Some(box_) = boxes.get_mut(&box_id) {
+                    box_.shift_remove(&label);
+                }
+            },
+            '=' => {
+                let focal_len: usize = split.next().unwrap().parse().unwrap();
+                let box_ = boxes.entry(box_id).or_default();
+                let lens = box_.entry(label).or_default();
+                *lens = focal_len;
+            },
+            _ => panic!("Expected - or ="),
+        }
+    }
+
+    let result2: usize = boxes.iter().map(|(box_num,b)| {
+        b.values().enumerate().map(|(lens_num, focal_len)|{
+            (box_num + 1) * (lens_num + 1) * focal_len
+        }).sum::<usize>()
+    }).sum();
+
+
+    (result1 as isize, result2 as isize)
 }
